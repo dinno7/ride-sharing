48061f19cd597763a23935b9973b9dcaeec424bf
dinno <tahadlrb7@gmail.com>
2026-05-10 20:12:49 +0330

    feat(gateway): add `echo` as http router & proxy to trip service to create trip

---
+++ services/api-gateway/cmd/http.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+
+	"github.com/labstack/echo/v5"
+)
+
+type Coordinate struct {
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+}
+
+type HTTPTripPreviewRequestPayload struct {
+	UserID      string     `json:"userID"      validate:"required"`
+	Pickup      Coordinate `json:"pickup"      validate:"required"`
+	Destination Coordinate `json:"destination" validate:"required"`
+}
+
+func tripPreview(c *echo.Context) error {
+	payload := new(HTTPTripPreviewRequestPayload)
+	if err := c.Bind(payload); err != nil {
+		return err
+	}
+	if err := c.Validate(payload); err != nil {
+		return err
+	}
+
+	// TODO: Send to service
+	client := new(http.Client)
+	jsonToService, _ := json.Marshal(payload)
+	reader := bytes.NewReader(jsonToService)
+	req, err := http.NewRequest(http.MethodPost, "http://trip-service:7000/preview", reader)
+	if err != nil {
+		c.Logger().Error("failed to create request to trup service", "error", err)
+		return echo.ErrInternalServerError.Wrap(
+			errors.New("something went wrong, please try again"),
+		)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		c.Logger().Error("failed to send request to trup service", "error", err)
+		return echo.ErrInternalServerError.Wrap(
+			errors.New("something went wrong, please try again"),
+		)
+	}
+
+	defer resp.Body.Close()
+	b, _ := io.ReadAll(resp.Body)
+
+	// TODO: Send success response
+	return c.JSONBlob(http.StatusOK, b)
+}
+
+// func tripPreview(w http.ResponseWriter, r *http.Request) {
+// 	r.Body = http.MaxBytesReader(w, r.Body, 5*1024*1024)
+// 	p, err := validator.ValidateHttpBody[HTTPTripPreviewRequestPayload](r)
+// 	if err != nil {
+// 		util.HttpValidationErrorResponse(w, err.Error())
+// 		return
+// 	}
+//
+// 	// TODO: Send to service
+// 	client := new(http.Client)
+// 	jsonToService, _ := json.Marshal(p)
+// 	reader := bytes.NewReader(jsonToService)
+// 	req, err := http.NewRequest(http.MethodPost, "http://trip-service:7000/preview", reader)
+// 	if err != nil {
+// 		util.ErrorResponse(
+// 			w,
+// 			http.StatusInternalServerError,
+// 			"Something went wrong, please try again",
+// 		)
+// 		return
+// 	}
+// 	resp, err := client.Do(req)
+// 	if err != nil {
+// 		util.ErrorResponse(
+// 			w,
+// 			http.StatusInternalServerError,
+// 			"Something went wrong, please try again",
+// 		)
+// 		return
+// 	}
+// 	defer resp.Body.Close()
+// 	b, _ := io.ReadAll(resp.Body)
+//
+// 	// TODO: Send success response
+// 	util.HttpOkResponse(w, "Trip created successfully", string(b))
+// }
---
+++ services/api-gateway/cmd/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	echocustoms "github.com/dinno7/ride-sharing/services/api-gateway/cmd/echo-customs"
+	"github.com/dinno7/ride-sharing/shared/env"
+	"github.com/labstack/echo/v5"
+	"github.com/labstack/echo/v5/middleware"
+)
+
+var httpAddr = env.GetString("HTTP_ADDR", ":7000")
+
+func main() {
+	e := echo.New()
+	e.Validator = echocustoms.NewEchoValidator()
+	e.HTTPErrorHandler = echocustoms.CustomHTTPErrorHandler
+	e.Use(middleware.BodyLimit(2_097_152)) // 2MB
+	// e.Use(middleware.CSRF())
+	e.Pre(middleware.RemoveTrailingSlash())
+
+	ctx := context.Background()
+	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM, syscall.SIGKILL)
+	defer cancel()
+
+	sc := echo.StartConfig{
+		Address:         httpAddr,
+		GracefulTimeout: time.Second * 3,
+		BeforeServeFunc: func(s *http.Server) error {
+			e.Logger.Info("Starting API Gateway")
+			return nil
+		},
+	}
+
+	e.POST("/trip/preview", tripPreview)
+
+	if err := sc.Start(ctx, e); err != nil {
+		e.Logger.Error("failed to start server", "error", err)
+	}
+
+	// mux := http.NewServeMux()
+
+	// mux.HandleFunc("POST /trip/preview", tripPreview)
+	//
+	// server := &http.Server{
+	// 	Addr:    httpAddr,
+	// 	Handler: mux,
+	// }
+	//
+	// if err := server.ListenAndServe(); err != nil {
+	// 	log.Fatalf("failed to server api gateway, %v", err)
+	// }
+}
