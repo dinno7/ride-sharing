85947a2426279ab702c6bf8d5d5c4418fd1d1ccf
dinno <tahadlrb7@gmail.com>
2026-05-19 00:54:52 +0330

    feat(trip): caclulate & save ridefares in previewing trip

--- services/trip-service/internal/application/ports/trip_service.go
+++ services/trip-service/internal/application/ports/trip_service.go
@@ -7,7 +7,16 @@ import (
 	"github.com/dinno7/ride-sharing/shared/types"
 )
 
+type PreviewTripOutput struct {
+	RideFares []*domain.RideFare
+	Route     *types.Route
+}
+
 type TripService interface {
 	CreateTrip(ctx context.Context, fare *domain.RideFare) (*domain.Trip, error)
-	PreviewTrip(ctx context.Context, pickup, destination *types.Coordinate) (*types.Route, error)
+	PreviewTrip(
+		ctx context.Context,
+		userID string,
+		pickup, destination *types.Coordinate,
+	) (*PreviewTripOutput, error)
 }
--- services/trip-service/internal/service/trip.go
+++ services/trip-service/internal/service/trip.go
@@ -7,6 +7,7 @@ import (
 	"github.com/dinno7/ride-sharing/services/trip-service/internal/application/ports"
 	"github.com/dinno7/ride-sharing/services/trip-service/internal/domain"
 	"github.com/dinno7/ride-sharing/shared/types"
+	"github.com/dinno7/ride-sharing/shared/util"
 )
 
 type tripService struct {
@@ -35,11 +36,78 @@ func (s *tripService) CreateTrip(
 
 func (s *tripService) PreviewTrip(
 	ctx context.Context,
+	userID string,
 	pickup, destination *types.Coordinate,
-) (*types.Route, error) {
-	routes, err := s.routeCalculator.CalcRoutes(pickup, destination)
+) (*ports.PreviewTripOutput, error) {
+	route, err := s.routeCalculator.CalcRoutes(pickup, destination)
 	if err != nil {
 		return nil, fmt.Errorf("failed to %w", err)
 	}
-	return routes, nil
+	// TODO: Estimate pkg price with route
+	estimatedPrices := estimatePackagePrice(route)
+	// TODO: Generate & Save to DB Trip fare
+	rideFares := make([]*domain.RideFare, len(estimatedPrices))
+	for i, rfp := range estimatedPrices {
+		rideFares[i] = &domain.RideFare{
+			ID:                util.GenRandomID(),
+			UserID:            userID,
+			PackageSlug:       rfp.PackageSlug,
+			TotalPriceInCents: rfp.TotalPriceInCents,
+		}
+	}
+
+	rideFares, err = s.repo.SaveRideFares(ctx, rideFares)
+	if err != nil {
+		return nil, err
+	}
+
+	return &ports.PreviewTripOutput{
+		RideFares: rideFares,
+		Route:     route,
+	}, nil
+}
+
+func estimatePackagePrice(route *types.Route) []*domain.RideFare {
+	baseFares := getBaseFares()
+	estimatedFares := make([]*domain.RideFare, len(baseFares))
+	for i, bf := range baseFares {
+		estimatedFares[i] = estimateFareForRoute(bf, route.Distance, route.Duration)
+	}
+	return estimatedFares
+}
+
+func estimateFareForRoute(baseFare *domain.RideFare, distance, duration float64) *domain.RideFare {
+	pricePerKM := 1.5
+	pricePerMinute := 0.25
+
+	distancePrice := pricePerKM * distance
+	durationPrice := pricePerMinute * duration
+
+	totalPrice := distancePrice + durationPrice + baseFare.TotalPriceInCents
+
+	return &domain.RideFare{
+		PackageSlug:       baseFare.PackageSlug,
+		TotalPriceInCents: totalPrice,
+	}
+}
+
+func getBaseFares() []*domain.RideFare {
+	return []*domain.RideFare{
+		{
+			PackageSlug:       "suv",
+			TotalPriceInCents: 200,
+		},
+		{
+			PackageSlug:       "sedan",
+			TotalPriceInCents: 350,
+		},
+		{
+			PackageSlug:       "van",
+			TotalPriceInCents: 400,
+		},
+		{
+			PackageSlug:       "luxury",
+			TotalPriceInCents: 1000,
+		},
+	}
 }
