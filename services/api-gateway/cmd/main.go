eee00eab012ca0a27f7de5b08c161e71cd8a260b
dinno <tahadlrb7@gmail.com>
2026-07-11 22:51:03 +0330

    feat(gateway): add queue consumers, forward anything

--- services/api-gateway/cmd/main.go
+++ services/api-gateway/cmd/main.go
@@ -9,15 +9,24 @@ import (
 	"time"
 
 	echocustoms "github.com/dinno7/ride-sharing/services/api-gateway/cmd/echo-customs"
+	"github.com/dinno7/ride-sharing/services/api-gateway/cmd/ws"
 	"github.com/dinno7/ride-sharing/shared/env"
+	"github.com/dinno7/ride-sharing/shared/logger"
+	messaging "github.com/dinno7/ride-sharing/shared/messaging/rabbitmq"
+	wshub "github.com/dinno7/ride-sharing/shared/ws"
 	"github.com/labstack/echo/v5"
 	"github.com/labstack/echo/v5/middleware"
 )
 
-var httpAddr = env.GetString("HTTP_ADDR", ":7000")
+var (
+	httpAddr = env.GetString("HTTP_ADDR", ":7000")
+	amqpURI  = env.GetString("AMQP_URL", "amqp://guest:guest@rabbitmq:5672/")
+)
 
 func main() {
 	e := echo.New()
+	appLogger := logger.NewSlogLogger(e.Logger)
+
 	e.Validator = echocustoms.NewEchoValidator()
 	e.HTTPErrorHandler = echocustoms.CustomHTTPErrorHandler
 	e.Use(middleware.BodyLimit(2_097_152)) // 2MB
@@ -34,12 +43,42 @@ func main() {
 		},
 	}
 
-	hub := NewHub()
-	go hub.Run()
-	e.GET("/ws/drivers", handleDriverWS(hub))
-	e.GET("/ws/riders", handleRiderWS(hub))
+	wsHub := wshub.NewHub()
+	go wsHub.Run()
+
+	messagingConn, err := messaging.NewRabbitMQBroker(amqpURI, appLogger)
+	if err != nil {
+		panic(err)
+	}
+	consumer, err := setupMessageConsumer(messagingConn, appLogger)
+	if err != nil {
+		panic(err)
+	}
+	// publisher := setupMessagePublisher(messagingConn)
+
+	driverWsHandler := ws.NewDriverWSHandler(wsHub, consumer)
+	riderWsHandler := ws.NewRiderWSHandler(wsHub)
+
+	// INFO: Consuming queues
+	messageConsumerHandler := NewMessageConsumerHandler(wsHub, appLogger)
+	consumeAndForwardQueues := []string{
+		DriverCmdTripRequestQueue,
+		NotifyNoDriversFoundQueue,
+		NotifyDriverAssignedQueue,
+	}
+	for _, queueName := range consumeAndForwardQueues {
+		if err = consumer.Consume(
+			queueName,
+			messageConsumerHandler.Forward,
+		); err != nil {
+			panic(err)
+		}
+	}
+
 	e.POST("/trip/preview", handleTripPreview)
 	e.POST("/trip/start", handleTripStart)
+	e.GET("/ws/drivers", driverWsHandler.Handle)
+	e.GET("/ws/riders", riderWsHandler.Handle)
 
 	ctx, cancel := signal.NotifyContext(
 		context.Background(),
@@ -47,6 +86,7 @@ func main() {
 		syscall.SIGTERM,
 	)
 	defer cancel()
+
 	if err := sc.Start(ctx, e); err != nil {
 		e.Logger.Error("failed to start server", "error", err)
 	}
